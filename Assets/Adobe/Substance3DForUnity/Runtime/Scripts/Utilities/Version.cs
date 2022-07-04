cd76741d7a57efeef45086e64b82a64024e84635
Liam Parker <72252979+Pentonize@users.noreply.github.com>
2022-07-04 16:32:09 +0100

    Added sand material and imported Substance plugin to unity

---
+++ Assets/Adobe/Substance3DForUnity/Runtime/Scripts/Utilities/Version.cs
@@ -0,0 +1,9 @@
+namespace Adobe.Substance
+{
+    public static class Version
+    {
+        public const string PluginVersion = "3.0.0-0-g535939d";
+        public const string EngineVersion = "8.2.0 0138644d";
+        public const int ImporterVersion = 300;
+    }
+}
